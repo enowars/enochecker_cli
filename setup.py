971f8e09c738858068e20ef6f6ff9466f2c2c0b7
Benedikt Radtke <benediktradtke@gmail.com>
2019-10-18 23:40:09 +0200

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as f:
 
 setuptools.setup(
     name='enochecker_cli',  
-    version='0.1.1',
+    version='0.1.2',
     entry_points = {
         "console_scripts": ['enochecker_cli = enochecker_cli:main']
     },
