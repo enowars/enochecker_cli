66805cdf536eb2e72971591e11e5c4761ddf204c
Lucas Druschke <ldruschk@posteo.de>
2021-04-23 10:06:28 +0200

    bump enochecker_core to 0.8.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as f:
 
 setuptools.setup(
     name='enochecker_cli',
-    version='0.5.0',
+    version='0.5.1',
     entry_points = {
         "console_scripts": ['enochecker_cli = enochecker_cli.base:main']
     },
